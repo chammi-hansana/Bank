2021055e3c6ac4e882136fc97b1e454bafc83570
Chammi Hansana <chammi2819@gmail.com>
2022-01-03 22:59:13 +0530

    void added

--- Bank_management_system.cpp
+++ Bank_management_system.cpp
@@ -1,8 +1,33 @@
 #include<stdio.h>
 #include<conio.h>
 #include<iostream>
-using namespace std;
-main()
+class bank{
+    char name[100],address[100],Y;
+    int balance;
+    public:
+    void open_account();
+    void deposite_money();
+    void withdraw_money();
+    void display_money();
+    };
+    void bank :: open_account(){
+        cout<<"Enter your full name :: ";
+        cin.ignore();
+        cin.getline(name,100);
+        cout<<"Enter your full address :: ";
+        cin.ignore();
+        cin.getline(address,100);
+        cout<<"What type of account you want to open saving (s) or current (c) ";
+        cin>>Y;
+        cout<<"Enter amount for deposite :: ";
+        cin>>balance;
+        cout<<"Your account is created \n";
+        }
+        void deposite :: diposite_money()
+        {
+            
+        }
+int main()
 {
     cout<<"1) Open account\n";
     cout<<"2) Deposite money\n";
