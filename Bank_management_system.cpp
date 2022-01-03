06144fc1615f1d1bdf21945cbce58a40663fb841
Chammi Hansana <chammi2819@gmail.com>
2022-01-03 22:13:35 +0530

    first commit

---
+++ Bank_management_system.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+using namespace std;
+main()
+{
+    cout<<"1) Open account\n";
+    cout<<"2) Deposite money\n";
+    cout<<"3) Withdraw money\n";
+    cout<<"4) Display account\n";
+    cout<<"5) Exit";
+
+    getch();
+    return 0;
+    
+}
+
