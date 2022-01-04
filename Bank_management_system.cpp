9a685c74b1b3ded280dea65d839300ceadffb266
Chammi Hansana <chammi2819@gmail.com>
2022-01-04 23:04:46 +0530

    do case use

--- Bank_management_system.cpp
+++ Bank_management_system.cpp
@@ -47,19 +47,44 @@ class bank{
               cout<<"\n withdraw :: ";
               cout<<"Enter amount to withdraw ::";
               cin>>amount;
+              balance = balance - amount;
+              cout<<"Now total amount is left :: "<<balance;
 
           }
 
-
-
-
 int main()
 {
+  int ch;
+  bank obj;
+  do{
+
     cout<<"1) Open account\n";
     cout<<"2) Deposite money\n";
     cout<<"3) Withdraw money\n";
     cout<<"4) Display account\n";
     cout<<"5) Exit";
+    cout<<"Select the option from above \n";
+    cin>>ch;
+    switch(ch){
+      case 1:<<"1) open account \n";
+      obj.open_account();
+      break;
+
+      case 2:<<"2) deposite money \n";
+      obj.deposite_money();
+      break;
+
+      case 3:
+         cout<<"3) withdraw money : \n";
+         obj.withdraw_money();
+         break;
+
+         case 4: "4) display account \n";
+         obj.display_account();
+         break;
+          case 5:
+          if()
+    }
 
     getch();
     return 0;
